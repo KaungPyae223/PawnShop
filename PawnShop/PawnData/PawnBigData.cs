53646b49c60e7daa392c86fef8635836dcb28b20
KaungPyae223 <kaungpyaeaung8123@gmail.com>
2023-01-05 19:29:31 +0630

    Show data with specisfic condition

--- PawnShop/PawnData/PawnBigData.cs
+++ PawnShop/PawnData/PawnBigData.cs
@@ -1,9 +1,7 @@
 ﻿using PawnShop.DBO;
 using System;
 using System.Data;
-using System.Data.SqlTypes;
 using System.Drawing;
-using System.Globalization;
 using System.Windows.Forms;
 
 namespace PawnShop.PawnData
@@ -25,32 +23,70 @@ namespace PawnShop.PawnData
 
         private void frmPawnBigData_Load(object sender, EventArgs e)
         {
-            
+
             DataTable DT;
-            SP = string.Format("SP_SelectPawn N'{0}',N'{1}',N'{2}'","0","0", "4");
+            SP = string.Format("SP_SelectPawn N'{0}',N'{1}',N'{2}'", "0", "0", "4");
             DT = objclsMain.SelectData(SP);
             dtpFrom.Text=DT.Rows[0][0].ToString();
             ShowData();
+
         }
         public void ShowData()
         {
-            SP = string.Format("SP_SelectPawn N'{0}',N'{1}',N'{2}'",dtpFrom.Text.ToString(),dtpTo.Text.ToString(), "3");
+            SP = string.Format("SP_SelectPawn N'{0}',N'{1}',N'{2}'", dtpFrom.Text.ToString(), dtpTo.Text.ToString(), "3");
             dgvPawn.DataSource = objclsMain.SelectData(SP);
             dgvPawn.Columns[0].Width = (dgvPawn.Width/100)*5;
             dgvPawn.Columns[1].Width = (dgvPawn.Width/100)*10;
             dgvPawn.Columns[2].Width = (dgvPawn.Width/100)*15;
             dgvPawn.Columns[3].Width = (dgvPawn.Width/100)*10;
-            dgvPawn.Columns[4].Width = (dgvPawn.Width/100)*18;
-            dgvPawn.Columns[5].Width = (dgvPawn.Width/100)*12;
+            dgvPawn.Columns[4].Width = (dgvPawn.Width/100)*20;
+            dgvPawn.Columns[5].Width = (dgvPawn.Width/100)*15;
             dgvPawn.Columns[6].Width = (dgvPawn.Width/100)*15;
-            dgvPawn.Columns[7].Visible = false;
-            dgvPawn.Columns[8].Visible = false;
-            dgvPawn.Columns[9].Width = (dgvPawn.Width/100)*20;
+            dgvPawn.Columns[7].Width = (dgvPawn.Width/100)*15;
 
-          
+            dgvPawn.Columns[8].Visible = false;
+            dgvPawn.Columns[9].Visible = false;
+
+            for (int i = 0; i<dgvPawn.RowCount-1; i++)
+            {
+                if (dgvPawn.Rows[i].Cells[7].Value.ToString().Length>0)
+                {
+                    dgvPawn.Rows[i].DefaultCellStyle.BackColor=Color.Aquamarine;
+                    dgvPawn.Rows[i].DefaultCellStyle.ForeColor=Color.Black;
+                }
+                else
+                {
+                    DataTable DT;
+                    SP = string.Format("SP_SelectPawn N'{0}',N'{1}',N'{2}'", "0", "0", "4");
+                    DT = objclsMain.SelectData(SP);
+                    int diff = DateTime.Compare(Convert.ToDateTime(dgvPawn.Rows[i].Cells[6].Value.ToString()), Convert.ToDateTime(DT.Rows[0][0].ToString()));
+                    if(diff < 0)
+                    {
+                        dgvPawn.Rows[i].DefaultCellStyle.BackColor=Color.Pink;
+                        dgvPawn.Rows[i].DefaultCellStyle.ForeColor=Color.Black;
+
+                    }
+
+                }
+                if (dgvPawn.Rows[i].Cells[8].Value.ToString() != null)
+                {
+                    if(dgvPawn.Rows[i].Cells[8].Value.ToString().Contains("Lost Ticket"))
+                    {
+                        dgvPawn.Rows[i].DefaultCellStyle.BackColor=Color.Red;
+                        dgvPawn.Rows[i].DefaultCellStyle.ForeColor=Color.White;
+                    }
+                    
+                    if (dgvPawn.Rows[i].Cells[8].Value.ToString().Contains("Lost Ticket") && dgvPawn.Rows[i].Cells[7].Value.ToString() != null)
+                    {
+                        dgvPawn.Rows[i].DefaultCellStyle.BackColor=Color.DarkBlue;
+                        dgvPawn.Rows[i].DefaultCellStyle.ForeColor=Color.White;
+                    }
+                }
+                
+
+            }
 
         }
-
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -59,12 +95,12 @@ namespace PawnShop.PawnData
 
         private void dtpFrom_ValueChanged(object sender, EventArgs e)
         {
-            
+
             DataTable DT;
             SP = string.Format("SP_SelectPawn N'{0}',N'{1}',N'{2}'", dtpFrom.Text, dtpTo.Text, "5");
             DT = objclsMain.SelectData(SP);
             int DateDiff = Convert.ToInt32(DT.Rows[0]["NO"]);
-            if(DateDiff<0)
+            if (DateDiff<0)
             {
                 MessageBox.Show("Plese check Date", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 dtpFrom.Focus();
@@ -91,7 +127,7 @@ namespace PawnShop.PawnData
         private void tsbEdit_Click(object sender, EventArgs e)
         {
             showEntry();
-            
+
         }
         public void showEntry()
         {
