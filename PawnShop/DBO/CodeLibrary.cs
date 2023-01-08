0030e4dcaa3dd4973e990386ce52708fbca49fd1
KaungPyae223 <kaungpyaeaung8123@gmail.com>
2023-01-08 10:57:27 +0630

    code library for dgvPawn is finished

--- PawnShop/DBO/CodeLibrary.cs
+++ PawnShop/DBO/CodeLibrary.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace PawnShop.DBO
@@ -26,6 +27,59 @@ namespace PawnShop.DBO
             return DT.Rows[0][0].ToString();
         }
        
-         
+         public void DGVpawnEdid(ref DataGridView dgvPawn)
+        {
+            dgvPawn.Columns[0].Width = (dgvPawn.Width/100)*5;
+            dgvPawn.Columns[1].Width = (dgvPawn.Width/100)*10;
+            dgvPawn.Columns[2].Width = (dgvPawn.Width/100)*15;
+            dgvPawn.Columns[3].Width = (dgvPawn.Width/100)*10;
+            dgvPawn.Columns[4].Width = (dgvPawn.Width/100)*18;
+            dgvPawn.Columns[5].Width = (dgvPawn.Width/100)*12;
+            dgvPawn.Columns[6].Width = (dgvPawn.Width/100)*15;
+            dgvPawn.Columns[7].Visible = false;
+            dgvPawn.Columns[8].Visible = false;
+            dgvPawn.Columns[9].Width = (dgvPawn.Width/100)*20;
+        }
+        public void MakeColors(ref DataGridView dgvPawn)
+        {
+            for (int i = 0; i<dgvPawn.RowCount-1; i++)
+            {
+                if (dgvPawn.Rows[i].Cells[7].Value.ToString().Length>0)
+                {
+                    dgvPawn.Rows[i].DefaultCellStyle.BackColor=Color.Aquamarine;
+                    dgvPawn.Rows[i].DefaultCellStyle.ForeColor=Color.Black;
+                }
+                else
+                {
+                    DataTable DT;
+                    SP = string.Format("SP_SelectPawn N'{0}',N'{1}',N'{2}','{3}'", "0", "0", "0", "4");
+                    DT = objclsMain.SelectData(SP);
+                    int diff = DateTime.Compare(Convert.ToDateTime(dgvPawn.Rows[i].Cells[6].Value.ToString()), Convert.ToDateTime(DT.Rows[0][0].ToString()));
+                    if (diff < 0)
+                    {
+                        dgvPawn.Rows[i].DefaultCellStyle.BackColor=Color.Pink;
+                        dgvPawn.Rows[i].DefaultCellStyle.ForeColor=Color.Black;
+
+                    }
+
+                }
+                if (dgvPawn.Rows[i].Cells[8].Value.ToString() != null)
+                {
+                    if (dgvPawn.Rows[i].Cells[8].Value.ToString().Contains("Lost Ticket"))
+                    {
+                        dgvPawn.Rows[i].DefaultCellStyle.BackColor=Color.Red;
+                        dgvPawn.Rows[i].DefaultCellStyle.ForeColor=Color.White;
+                    }
+
+                    if (dgvPawn.Rows[i].Cells[8].Value.ToString().Contains("Lost Ticket") && dgvPawn.Rows[i].Cells[7].Value.ToString() != null)
+                    {
+                        dgvPawn.Rows[i].DefaultCellStyle.BackColor=Color.DarkBlue;
+                        dgvPawn.Rows[i].DefaultCellStyle.ForeColor=Color.White;
+                    }
+                }
+
+
+            }
+        }
     }
 }
--- PawnShop/PawnData/PawnBigData.cs
+++ PawnShop/PawnData/PawnBigData.cs
@@ -28,7 +28,7 @@ namespace PawnShop.PawnData
 
             dtpFrom.Text=objclsCodeLibrary.LastSixMonthes();
             SP = string.Format("SP_SelectPawn N'{0}',N'{1}',N'{2}','{3}'", dtpFrom.Text.ToString(), dtpTo.Text.ToString(), "0", "3");
-            objclsMain.toolStripTextBoxdata(ref toolStripTextBox1, SP, "ItemName");
+            objclsMain.toolStripTextBoxdata(ref toolStripTextBox1, SP, "CustomerName");
             ShowData();
 
         }
@@ -36,64 +36,16 @@ namespace PawnShop.PawnData
         {
             SP = string.Format("SP_SelectPawn N'{0}',N'{1}',N'{2}','{3}'", dtpFrom.Text.ToString(), dtpTo.Text.ToString(),"0", "3");
             dgvPawn.DataSource = objclsMain.SelectData(SP);
-            dgvPawn.Columns[0].Width = (dgvPawn.Width/100)*5;
-            dgvPawn.Columns[1].Width = (dgvPawn.Width/100)*10;
-            dgvPawn.Columns[2].Width = (dgvPawn.Width/100)*15;
-            dgvPawn.Columns[3].Width = (dgvPawn.Width/100)*10;
-            dgvPawn.Columns[4].Width = (dgvPawn.Width/100)*18;
-            dgvPawn.Columns[5].Width = (dgvPawn.Width/100)*12;
-            dgvPawn.Columns[6].Width = (dgvPawn.Width/100)*15;
-            dgvPawn.Columns[7].Visible = false;
-            dgvPawn.Columns[8].Visible = false;
-            dgvPawn.Columns[9].Width = (dgvPawn.Width/100)*20;
+            objclsCodeLibrary.DGVpawnEdid(ref dgvPawn);
 
-            MakeColors();
+            objclsCodeLibrary.MakeColors(ref dgvPawn);
             
         }
-        public void MakeColors()
-        {
-            for (int i = 0; i<dgvPawn.RowCount-1; i++)
-            {
-                if (dgvPawn.Rows[i].Cells[7].Value.ToString().Length>0)
-                {
-                    dgvPawn.Rows[i].DefaultCellStyle.BackColor=Color.Aquamarine;
-                    dgvPawn.Rows[i].DefaultCellStyle.ForeColor=Color.Black;
-                }
-                else
-                {
-                    DataTable DT;
-                    SP = string.Format("SP_SelectPawn N'{0}',N'{1}',N'{2}','{3}'", "0", "0", "0", "4");
-                    DT = objclsMain.SelectData(SP);
-                    int diff = DateTime.Compare(Convert.ToDateTime(dgvPawn.Rows[i].Cells[6].Value.ToString()), Convert.ToDateTime(DT.Rows[0][0].ToString()));
-                    if (diff < 0)
-                    {
-                        dgvPawn.Rows[i].DefaultCellStyle.BackColor=Color.Pink;
-                        dgvPawn.Rows[i].DefaultCellStyle.ForeColor=Color.Black;
-
-                    }
-
-                }
-                if (dgvPawn.Rows[i].Cells[8].Value.ToString() != null)
-                {
-                    if (dgvPawn.Rows[i].Cells[8].Value.ToString().Contains("Lost Ticket"))
-                    {
-                        dgvPawn.Rows[i].DefaultCellStyle.BackColor=Color.Red;
-                        dgvPawn.Rows[i].DefaultCellStyle.ForeColor=Color.White;
-                    }
-
-                    if (dgvPawn.Rows[i].Cells[8].Value.ToString().Contains("Lost Ticket") && dgvPawn.Rows[i].Cells[7].Value.ToString() != null)
-                    {
-                        dgvPawn.Rows[i].DefaultCellStyle.BackColor=Color.DarkBlue;
-                        dgvPawn.Rows[i].DefaultCellStyle.ForeColor=Color.White;
-                    }
-                }
-
-
-            }
-        }
+        
         private void button1_Click(object sender, EventArgs e)
         {
-            MakeColors();
+            objclsCodeLibrary.MakeColors(ref dgvPawn);
+
         }
 
         private void dtpFrom_ValueChanged(object sender, EventArgs e)
@@ -171,7 +123,7 @@ namespace PawnShop.PawnData
                 SP = string.Format("SP_SelectPawn N'{0}',N'{1}',N'{2}','{3}'", dtpFrom.Text.ToString(), dtpTo.Text.ToString(), toolStripTextBox1.Text, "9");
 
             dgvPawn.DataSource = objclsMain.SelectData(SP);
-            MakeColors();
+            objclsCodeLibrary.MakeColors(ref dgvPawn);
 
         }
 
